87a610e57372fc3bbab8e65c0b19624ba38934e0
chenlichao <chenlichao-khjy@bestpay.com.cn>
2020-05-14 15:41:25 +0800

    fix: cookie

--- src/cookie/index.js
+++ src/cookie/index.js
@@ -25,8 +25,8 @@ export const cookie = {
     return null;
   },
   clear (key) {
-    document.cookie = 'name=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.cookie = 'name=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/';
+    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/`;
     const res = this.get(key);
     return !res;
   }
