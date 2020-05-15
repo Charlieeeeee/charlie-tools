b4ea281dca8fc2bf86f45b178d795615a3c07fbc
chenlichao <chenlichao-khjy@bestpay.com.cn>
2020-05-15 11:12:52 +0800

    feat: add copy

--- develop.md
+++
@@ -1,7 +0,0 @@
-### 发布流程
-
-> git add .
-> git commit -m ""
-> git push
-> git version patch
-> git publish --access=public
---
+++ publish.sh
@@ -0,0 +1,11 @@
+### 发布流程
+
+git add .
+
+git commit -m "$1"
+
+git push
+
+git version patch
+
+git publish --access=public
