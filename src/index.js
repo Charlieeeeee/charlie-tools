5627ecbf9731612173a0e8b68e309573a663ff63
chenlichao <chenlichao-khjy@bestpay.com.cn>
2020-06-04 21:07:12 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-export { cookie } from './cookie';
-export { addListener, removeListener } from './listener';
-export { copy } from './copy';
-export { timeFormat } from './timeFormat';
-export { groupArray } from './groupArray';
-export { getHash } from './getHash';
+// export { cookie } from './cookie';
+// export { addListener, removeListener } from './listener';
+// export { copy } from './copy';
+// export { timeFormat } from './timeFormat';
+// export { groupArray } from './groupArray';
+// export { getHash } from './getHash';
+
+import { cookie } from './cookie';
+import { addListener, removeListener } from './listener';
+import { copy } from './copy';
+import { timeFormat } from './timeFormat';
+import { groupArray } from './groupArray';
+import { getHash } from './getHash';
+
+export {
+    cookie,
+    addListener,
+    removeListener,
+    copy,
+    timeFormat,
+    groupArray,
+    getHash
+}
 export default {
     cookie,
     addListener,
