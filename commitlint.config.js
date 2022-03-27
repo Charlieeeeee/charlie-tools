7765147294a275ae912cb281a06b661d05625b0d
陈礼超 <977503861@qq.com>
2022-03-28 03:10:55 +0800

    chore: 1.添加了需求；2.修改了commitlint配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert'],
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'build'],
     ],
     'subject-full-stop': [0, 'never'],
     'subject-case': [0, 'never'],
