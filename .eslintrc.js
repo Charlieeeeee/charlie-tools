60b12e8f98301c67699ceeeb1facc8a00a1b6f7b
陈礼超 <977503861@qq.com>
2022-04-02 01:07:09 +0800

    chore: check lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,39 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
   },
-  extends: [
-    'airbnb-base',
-    'prettier',
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
   },
-  plugins: [
-    '@typescript-eslint',
+  'plugins': [
+    '@typescript-eslint'
   ],
-  rules: {
-    quotes: ['error', 'single', { 'avoidEscape': true }],
-    'quote-props': 0,
-    'no-console': 0,
-    'no-param-reassign': 0,
-  },
-};
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-trailing-spaces': 'error',
+    'arrow-spacing': 'error',
+    'eol-last': 'error', // 非空文件中跟踪换行符
+    'keyword-spacing': 'error',
+    'key-spacing': 'error',
+    'space-infix-ops': 'error', // 这条规则旨在确保中缀操作员周围有空间。
+  }
+}
--- commitlint.config.js
+++ commitlint.config.js
@@ -9,4 +9,4 @@ module.exports = {
     'subject-full-stop': [0, 'never'],
     'subject-case': [0, 'never'],
   },
-};
+}
