9056629bf03c223794dd69626cf94df9bdda6bc5
陈礼超 <977503861@qq.com>
2022-04-04 02:24:35 +0800

    fix: 声明文件

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,9 +4,4 @@ interface IAnyObj {
 export declare const getQueryObject: (url: string) => IAnyObj
 export declare const copyTexts: (texts: string) => Promise<void>
 export declare const formatTime: (date: Date | string | number, fmt: string) => string
-declare const ct: {
-    getQueryObject: (url: string) => IAnyObj;
-    copyTexts: (texts: string) => Promise<void>;
-    formatTime: (date: Date | string | number, fmt: string) => string;
-}
-export default ct
+export {}
