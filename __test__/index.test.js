10d0dc697724e42b9a0d87c55741fcf8ec642068
陈礼超 <977503861@qq.com>
2022-04-03 23:30:28 +0800

    ci: update .travis.yml&index.test.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -10,7 +10,6 @@ test('用 formatTime 格式化时间', () => {
   const t1 = ct.formatTime(new Date('2022/12/12 10:00:00'), 'yyyy-MM-dd hh:mm:ss')
   const t2 = ct.formatTime(+new Date('2022/12/12 10:00:00'), 'yyyy-MM-dd hh:mm:ss')
   const t3 = ct.formatTime('2022/12/12 10:00:00', 'yyyy-MM-dd hh:mm:ss')
-  console.log(t1)
   expect(t1).toBe('2022-12-12 10:00:00')
   expect(t2).toBe('2022-12-12 10:00:00')
   expect(t3).toBe('2022-12-12 10:00:00')
